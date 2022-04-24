0229654b91abd6d20e2d4cbbe898c95b5c74a6f5
NguyenThu <nguyenthu753951@gmail.com>
2022-04-24 20:07:21 +0700

    tao chatbot

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
+import re
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+# from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 
-app = Flask(__name__)
+chatbot = ChatBot("Ron Obvious")
 
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = ChatterBotCorpusTrainer(english_bot)
-trainer.train("chatterbot.corpus.english")
+app = Flask(__name__, template_folder='template')
+
+# english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+# trainer = ChatterBotCorpusTrainer(english_bot)
+# trainer.train("chatterbot.corpus.english")
+
+# conversation = [
+#     "Hello",
+#     "Hi there!",
+#     "How are you doing?",
+#     "I'm doing great.",
+#     "That is good to hear",
+#     "Thank you.",
+#     "You're welcome.",
+#     "Toi la Thu",
+#     "Xin chao Thu, toi la chatbot."
+# ]
+
+# trainer = ListTrainer(chatbot)
+
+# trainer.train(conversation)
 
 @app.route("/")
 def home():
@@ -15,7 +35,19 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    # return str(english_bot.get_response(userText))
+    print('response' + str(chatbot.get_response(userText)))
+    if "/" in userText:
+        conversation = userText.split("/")
+        training(conversation)
+        return "try again!"
+    return str(chatbot.get_response(userText))
+
+def training(conversation):
+    print("Tranning for chatbot")
+    trainer = ListTrainer(chatbot)
+    trainer.train(conversation)
+
 
 
 if __name__ == "__main__":
--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\Desktop\chatbot\venv"
+$env:VIRTUAL_ENV="D:\DA_CoSo\Chatbot\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
